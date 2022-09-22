1648ea20d6e11801f93fdcda6feac4ece5d8625c
Hillary Muller <hillaryaward@gmail.com>
2022-09-22 14:28:44 -0500

    commit tree e99f76be1d74bb6cd5dc0f6cc4787c7f35991366

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,14 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    matches = students.filter(params[:name] == student.first_name || student.last_name)
     render json: students
+    render json: matches
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
